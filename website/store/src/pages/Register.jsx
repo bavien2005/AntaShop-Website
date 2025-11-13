488aba2ca0241ac5b85764c46d36cac58f96b618
QangThun <phamquangthuan3107@gmail.com>
2025-11-13 21:45:30 +0700

    update login

--- website/store/src/pages/Register.jsx
+++ website/store/src/pages/Register.jsx
@@ -305,7 +305,7 @@ export default function Register() {
                 </div>
 
                 <div className="form-section">
-                  <h2 className="section-title">
+                  <h2 className="section-title" style={{ display: 'inline-flex', alignItems: 'center', gap: '25px' }}>
                     <svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
                       <rect x="3" y="11" width="18" height="11" rx="2" ry="2"></rect>
                       <path d="M7 11V7a5 5 0 0 1 10 0v4"></path>
